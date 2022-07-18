bbd6d05f71b1a13ada1cb6be18fe7802f4ca3f8e
Kateria Wynn <iamkateria@gmail.com>
2022-07-17 18:43:48 -0700

    added event delegation example

--- js-events-code-along/app.js
+++ js-events-code-along/app.js
@@ -1,1 +1,22 @@
+//Event delegation example
+// const form = document.querySelector('form');
+// const friendList = document.querySelector('#friend-list');
 
+// friendList.addEventListener('click', function (e) {
+//   if (e.target.tagName === 'BUTTON') {
+//     e.target.parentElement.remove();
+//   }
+// });
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   const newFriendInput = document.querySelector('#first-name');
+//   const newLi = document.createElement('li');
+//   const newButton = document.createElement('button');
+//   newLi.innerText = newFriendInput.value;
+//   newButton.innerText = 'Remove';
+
+//   newLi.append(newButton);
+//   friendList.append(newLi);
+//   form.reset();
+// });
